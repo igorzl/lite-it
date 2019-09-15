adbda2c76f25d912fbb02e2049ec96f32d8c3cc8
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-15 17:54:24 +0300

    fabric installed. lock and load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
-import "bootstrap";
+import 'bootstrap';
+import { fabric } from 'fabric';
+
+var canvas = new fabric.Canvas('canvas');
+
+var rect = new fabric.Rect({
+  top: 100,
+  left: 100,
+  width: 60,
+  height: 70,
+  fill: 'red'
+});
+
+canvas.add(rect);
