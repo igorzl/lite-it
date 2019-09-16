a734170b8a427ae2012ac02f3a747bf030e763f5
IgorZ <igor_zlobinski@bezeqint.net>
2019-09-16 12:28:38 +0300

    Pundit with project and canvas policies created (wrong field names fixed)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
   end
 
   # Pundit staff
