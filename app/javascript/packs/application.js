81daf019245eef063080464798056495554ce8cf
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-16 13:10:41 +0300

    buttons are working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import { fabric } from 'fabric';
 
 var canvas = new fabric.Canvas('canvas');
 
+// Adding objects
 var rect = new fabric.Rect({
   top: 100,
   left: 100,
@@ -11,4 +12,26 @@ var rect = new fabric.Rect({
   fill: 'red'
 });
 
-canvas.add(rect);
+function addRectangle() {
+  canvas.add(rect);
+  console.log(rect);
+}
+
+let addRectBtn = document.getElementById('rectangle-btn');
+addRectBtn.addEventListener('click', addRectangle);
+
+// Adding text
+
+var text = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+  left: 50,
+  top: 50,
+  width: 150,
+  fontSize: 20
+});
+
+function addTextbox() {
+  canvas.add(text);
+}
+
+let addTextBtn = document.getElementById('text-btn');
+addTextBtn.addEventListener('click', addTextbox);
