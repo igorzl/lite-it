2080d19510f17db27f074f89b5c8a1840b3c8ec8
Alisa Givertz <alisagivertz@gmail.com>
2019-09-18 11:00:17 +0300

    fixed routes and checking formats

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:index, :show, :destroy]
+  resources :projects, only: [:index, :show, :create, :new, :edit, :update, :destroy] do
+    resources :canvases, only: [:create, :show]
+  end
 end
