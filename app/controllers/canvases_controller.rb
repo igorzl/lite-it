ad0cc4396a08b3ce0917f7248b724c2c54e9ae56
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-18 11:26:26 +0300

    merging new master

--- app/controllers/canvases_controller.rb
+++ app/controllers/canvases_controller.rb
@@ -1,2 +1,29 @@
 class CanvasesController < ApplicationController
+  before_action :find_canvas, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @canvas = Canvas.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+
+  private
+
+  def find_geek
+    @geek = Canvas.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:index, :show, :destroy]
-end
+  resources :projects, only: [:index, :show, :destroy] do
+    resources :canvases, only: [:new, :create, :show]
+  end
+ end
