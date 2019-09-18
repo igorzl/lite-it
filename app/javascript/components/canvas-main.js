0b6642727497d32d46b295c045789a5470375d95
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-18 15:32:09 +0300

    save and load are working

--- app/javascript/components/canvas-main.js
+++ app/javascript/components/canvas-main.js
@@ -2,8 +2,40 @@ import { fabric } from 'fabric';
 
 var canvas = new fabric.Canvas('canvas');
 
-// Adding objects
+// Loading canvas New or from DB
+let loadCanvas = () => {
+  let canvasData = document.querySelector('#canvas').dataset;
+  if (canvasData.canvasJson != null) {
+    canvas.loadFromJSON(canvasData.canvasJson, function() {
+      canvas.renderAll();
+      console.log('Load from db');
+    });
+  } else {
+    console.log('Load new');
+    var group = [];
+    fabric.loadSVGFromURL(
+      '/assets/photo-camera.svg',
+      function(objects, options) {
+        var loadedObjects = new fabric.Group(group);
+        loadedObjects.set({
+          left: 480,
+          top: 500,
+          scaleX: 0.1,
+          scaleY: 0.1
+        });
+        canvas.add(loadedObjects);
+        canvas.renderAll();
+      },
+      function(item, object) {
+        object.set('id', item.getAttribute('id'));
+        group.push(object);
+      }
+    );
+  }
+};
+loadCanvas();
 
+// Adding objects
 function addRectangle() {
   var rect = new fabric.Rect({
     top: 100,
@@ -47,43 +79,46 @@ function removeSelected() {
 let addDeleteBtn = document.getElementById('delete-btn');
 addDeleteBtn.addEventListener('click', removeSelected);
 
-// Parsing svg to the canvas
-var group = [];
-fabric.loadSVGFromURL(
-  '../../../assets/photo-camera.svg',
-  function(objects, options) {
-    var loadedObjects = new fabric.Group(group);
-    loadedObjects.set({
-      left: 480,
-      top: 500,
-      scaleX: 0.1,
-      scaleY: 0.1
-    });
-    canvas.add(loadedObjects);
-    canvas.renderAll();
-  },
-  function(item, object) {
-    object.set('id', item.getAttribute('id'));
-    group.push(object);
-  }
-);
-
 // Saving canvas to JSON string
-
-let addSaveBtn = document.getElementById('save-btn');
-addSaveBtn.addEventListener('click', saveCanvas);
-
 function saveCanvas() {
-  var json = JSON.stringify(canvas.toJSON());
-  console.log(json);
+  return JSON.stringify(canvas.toJSON());
 }
 
 // Save as an SVG image
-
 function rasterize() {
-  const svgSnapshot = canvas.toSVG();
-  document.getElementById('bottom-navigation').innerHTML = svgSnapshot;
+  return canvas.toSVG();
 }
 
-let addSvgBtn = document.getElementById('svg-btn');
-addSvgBtn.addEventListener('click', rasterize);
+let addSaveBtn = document.getElementById('save-btn');
+addSaveBtn.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const canvas_json = document.querySelector('#canvas_json');
+  const canvas_svg = document.querySelector('#canvas_svg');
+
+  canvas_json.value = saveCanvas();
+  canvas_svg.value = rasterize();
+
+  addSaveBtn.submit();
+});
+
+// Parsing svg to the canvas
+// var group = [];
+// fabric.loadSVGFromURL(
+//   '/assets/photo-camera.svg',
+//   function(objects, options) {
+//     var loadedObjects = new fabric.Group(group);
+//     loadedObjects.set({
+//       left: 480,
+//       top: 500,
+//       scaleX: 0.1,
+//       scaleY: 0.1
+//     });
+//     canvas.add(loadedObjects);
+//     canvas.renderAll();
+//   },
+//   function(item, object) {
+//     object.set('id', item.getAttribute('id'));
+//     group.push(object);
+//   }
+// );
