30a138155f5bdbf89bfcd621a8cddbceb38af626
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-23 18:32:10 +0300

    flip cards

--- app/javascript/components/canvas-view.js
+++ app/javascript/components/canvas-view.js
@@ -1,13 +1,16 @@
-import { fabric } from 'fabric';
+// Parsing SVG to the page with the element that contains id="#canas-svg"
+// Just add to the page this:
+// <div id="render-svg" data-canvas-svg="<%= @canvas.canvas_svg %>" style="background: white;"></div>
+// Width and all sizing is controlled via css by the svg {} selector.
 
-var canvas = new fabric.Canvas('canvas', {
-  isDrawingMode: false
-});
-let canvasData = document.querySelector('#canvas').dataset;
-canvas.loadFromJSON(
-  canvasData.canvasJson,
-  canvas.renderAll.bind(canvas),
-  function(o, object) {
-    object.set('selectable', false);
-  }
-);
+let loadSvg = () => {
+  let canvas_svg = document.querySelectorAll('#render-svg');
+  canvas_svg.forEach(cnvs => {
+    var doc = new DOMParser().parseFromString(
+      cnvs.dataset.canvasSvg,
+      'image/svg+xml'
+    );
+    cnvs.appendChild(cnvs.ownerDocument.importNode(doc.documentElement, true));
+  });
+};
+if (document.querySelector('#render-svg')) loadSvg();
