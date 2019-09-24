bc60967f1ca2534054410982a955a99f084e478a
IgorZ <igor_zlobinski@bezeqint.net>
2019-09-24 09:14:22 +0300

    Notes field changed place

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2019_09_23_144916) do
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "notes"
     t.string "state_json"
     t.string "canvas_svg"
-    t.text "notes"
     t.index ["project_id"], name: "index_canvases_on_project_id"
   end
 
